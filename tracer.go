6c4a08d98dd6b22cf73d8f911cdcddf7b4fb2b87
Ichinose Shogo <shogo82148@gmail.com>
2015-06-07 14:44:28 +0900

    tracer skips call stack based on filename

--- tracer.go
+++ tracer.go
@@ -3,18 +3,60 @@ package proxy
 import (
 	"database/sql/driver"
 	"fmt"
+	"runtime"
+	"strings"
 	"time"
 )
 
 // Outputter is what is used by the tracing proxy created via `NewTraceProxy`.
 // Anything that implements a `log.Logger` style `Output` method will satisfy
 // this interface.
-type Outputter interface{
+type Outputter interface {
 	Output(calldepth int, s string) error
 }
 
+type Filter interface {
+	DoOutput(file string) bool
+}
+
+type defaultFilter struct{}
+
+func (_ defaultFilter) DoOutput(file string) bool {
+	// skip database/sql
+	switch {
+	case strings.HasPrefix(file, "database/sql/"):
+	case strings.HasPrefix(file, "github.com/shogo82148/txmanager/"):
+	default:
+		return true
+	}
+	return false
+}
+
+func findCaller(f Filter) int {
+	// i starts 4 because 0: findCaller, 1: hooks, 2: proxy-funcs, 3: database/sql, and equals or greater than 4: user-funcs
+	for i := 4; ; i++ {
+		_, file, _, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+
+		srcIndex := strings.LastIndex(file, "/src/")
+		if srcIndex < 0 {
+			return i
+		}
+		if f.DoOutput(file[srcIndex+5:]) {
+			return i
+		}
+	}
+	return 0
+}
+
 // NewTraceProxy generates a proxy that logs queries.
-func NewTraceProxy(d driver.Driver, o Outputter) *Proxy {
+func NewTraceProxy(d driver.Driver, o Outputter, f Filter) *Proxy {
+	if f == nil {
+		f = defaultFilter{}
+	}
+
 	return &Proxy{
 		Driver: d,
 		Hooks: &Hooks{
@@ -23,7 +65,7 @@ func NewTraceProxy(d driver.Driver, o Outputter) *Proxy {
 			},
 			PostOpen: func(ctx interface{}, _ driver.Conn) error {
 				o.Output(
-					7,
+					findCaller(f),
 					fmt.Sprintf(
 						"Open (%s)",
 						time.Since(ctx.(time.Time)),
@@ -36,7 +78,7 @@ func NewTraceProxy(d driver.Driver, o Outputter) *Proxy {
 			},
 			PostExec: func(ctx interface{}, stmt *Stmt, args []driver.Value, _ driver.Result) error {
 				o.Output(
-					7,
+					findCaller(f),
 					fmt.Sprintf(
 						"Exec: %s; args = %v (%s)",
 						stmt.QueryString,
@@ -51,7 +93,7 @@ func NewTraceProxy(d driver.Driver, o Outputter) *Proxy {
 			},
 			PostQuery: func(ctx interface{}, stmt *Stmt, args []driver.Value, _ driver.Rows) error {
 				o.Output(
-					9,
+					findCaller(f),
 					fmt.Sprintf(
 						"Query: %s; args = %v (%s)",
 						stmt.QueryString,
@@ -66,7 +108,7 @@ func NewTraceProxy(d driver.Driver, o Outputter) *Proxy {
 			},
 			PostBegin: func(ctx interface{}, _ *Conn) error {
 				o.Output(
-					6,
+					findCaller(f),
 					fmt.Sprintf("Begin (%s)", time.Since(ctx.(time.Time))),
 				)
 				return nil
@@ -76,7 +118,7 @@ func NewTraceProxy(d driver.Driver, o Outputter) *Proxy {
 			},
 			PostCommit: func(ctx interface{}, _ *Tx) error {
 				o.Output(
-					7,
+					findCaller(f),
 					fmt.Sprintf("Commit (%s)", time.Since(ctx.(time.Time))),
 				)
 				return nil
@@ -86,7 +128,7 @@ func NewTraceProxy(d driver.Driver, o Outputter) *Proxy {
 			},
 			PostRollback: func(ctx interface{}, _ *Tx) error {
 				o.Output(
-					8,
+					findCaller(f),
 					fmt.Sprintf("Rollback (%s)", time.Since(ctx.(time.Time))),
 				)
 				return nil
