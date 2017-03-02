4de687bb0d8a35c5c7e2a4776bd18e3f21b7f73c
Ichinose Shogo <shogo82148@gmail.com>
2017-03-02 19:00:43 +0900

    speed up tracer

--- tracer.go
+++ tracer.go
@@ -36,27 +36,33 @@ var DefaultPackageFilter = PackageFilter{
 }
 
 func findCaller(f Filter) int {
-	// i starts 4. 0: findCaller, 1: hooks, 2: proxy-funcs, 3: database/sql, and equals or greater than 4: user-funcs
-	for i := 4; ; i++ {
-		pc, _, _, ok := runtime.Caller(i)
-		if !ok {
-			break
-		}
+	// skip starts 4. 0: Callers, 1: findCaller, 2: hooks, 3: proxy-funcs, 4: database/sql, and equals or greater than 5: user-funcs
+	skip := 5
+	for {
+		var rpc [8]uintptr
+		n := runtime.Callers(skip, rpc[:])
 
-		// http://stackoverflow.com/questions/25262754/how-to-get-name-of-current-package-in-go
-		name := runtime.FuncForPC(pc).Name()
-		dotIdx := 0
-		for j := len(name) - 1; j >= 0; j-- {
-			if name[j] == '.' {
-				dotIdx = j
-			} else if name[j] == '/' {
-				break
+		for i, pc := range rpc {
+			// http://stackoverflow.com/questions/25262754/how-to-get-name-of-current-package-in-go
+			name := runtime.FuncForPC(pc).Name()
+			dotIdx := 0
+			for j := len(name) - 1; j >= 0; j-- {
+				if name[j] == '.' {
+					dotIdx = j
+				} else if name[j] == '/' {
+					break
+				}
+			}
+			packageName := name[:dotIdx]
+			if f.DoOutput(packageName) {
+				// -1 because the meaning of skip differs between Caller and Callers.
+				return skip + i - 1
 			}
 		}
-		packageName := name[:dotIdx]
-		if f.DoOutput(packageName) {
-			return i
+		if n < len(rpc) {
+			break
 		}
+		skip += n
 	}
 	return 0
 }
