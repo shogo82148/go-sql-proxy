7c2f1ecdfac738dca6534649f3dba31b175a8966
Ichinose Shogo <shogo82148@gmail.com>
2019-12-31 18:15:33 +0900

    drop go 1.0

--- conn.go
+++ conn.go
@@ -295,3 +295,29 @@ func (conn *Conn) QueryContext(c context.Context, query string, args []driver.Na
 
 	return rows, nil
 }
+
+// ResetSession resets the state of Conn.
+func (conn *Conn) ResetSession(ctx context.Context) error {
+	var err error
+	var myctx interface{}
+	hooks := conn.Proxy.getHooks(ctx)
+
+	if hooks != nil {
+		defer func() { hooks.postResetSession(ctx, myctx, conn, err) }()
+		if myctx, err = hooks.preResetSession(ctx, conn); err != nil {
+			return err
+		}
+	}
+
+	if sr, ok := conn.Conn.(driver.SessionResetter); ok {
+		err = sr.ResetSession(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	if hooks != nil {
+		err = hooks.resetSession(ctx, myctx, conn)
+	}
+	return err
+}
