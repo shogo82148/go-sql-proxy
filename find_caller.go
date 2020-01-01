711abd853f860e63c3e6ef6a182238cc78c60bc2
Ichinose Shogo <shogo82148@gmail.com>
2020-01-01 15:55:03 +0900

    fix findCaller for Go1.11

---
+++ find_caller.go
@@ -0,0 +1,46 @@
+// +build go1.12
+
+package proxy
+
+import (
+	"runtime"
+)
+
+func findCaller(f Filter) int {
+	// skip starts 4. 0: Callers, 1: findCaller, 2: hooks, 3: proxy-funcs, 4: database/sql, and equals or greater than 5: user-funcs
+	skip := 5
+	for {
+		var rpc [8]uintptr
+		var i int
+		n := runtime.Callers(skip, rpc[:])
+		frames := runtime.CallersFrames(rpc[:])
+		for i = 0; ; i++ {
+			frame, more := frames.Next()
+			if !more {
+				break
+			}
+			name := frame.Function
+			if name == "" {
+				continue
+			}
+			// http://stackoverflow.com/questions/25262754/how-to-get-name-of-current-package-in-go
+			dotIdx := 0
+			for j := len(name) - 1; j >= 0; j-- {
+				if name[j] == '.' {
+					dotIdx = j
+				} else if name[j] == '/' {
+					break
+				}
+			}
+			packageName := name[:dotIdx]
+			if f.DoOutput(packageName) {
+				return skip + i
+			}
+		}
+		if n < len(rpc) {
+			break
+		}
+		skip += i
+	}
+	return 0
+}
---
+++ find_caller_go111.go
@@ -0,0 +1,39 @@
+// +build !go1.12
+
+package proxy
+
+import (
+	"runtime"
+)
+
+func findCaller(f Filter) int {
+	// skip starts 4. 0: Callers, 1: findCaller, 2: hooks, 3: proxy-funcs, 4: database/sql, and equals or greater than 5: user-funcs
+	skip := 5
+	for {
+		var rpc [8]uintptr
+		n := runtime.Callers(skip, rpc[:])
+
+		for i, pc := range rpc[:n] {
+			// http://stackoverflow.com/questions/25262754/how-to-get-name-of-current-package-in-go
+			name := runtime.FuncForPC(pc).Name()
+			dotIdx := 0
+			for j := len(name) - 1; j >= 0; j-- {
+				if name[j] == '.' {
+					dotIdx = j
+				} else if name[j] == '/' {
+					break
+				}
+			}
+			packageName := name[:dotIdx]
+			if f.DoOutput(packageName) {
+				// -1 because the meaning of skip differs between Caller and Callers.
+				return skip + i - 1
+			}
+		}
+		if n < len(rpc) {
+			break
+		}
+		skip += n
+	}
+	return 0
+}
